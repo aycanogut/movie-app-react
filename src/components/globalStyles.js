1f7eedbdb2996171bac3081200cd10bc9bc5ec34
aycanogut <aycanonfire@gmail.com>
2021-11-02 15:21:41 +0300

    add globalStyles

---
+++ src/components/globalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+
+// global styles
+html {
+    font-size: 62.5%;
+}
+
+body {
+height: 100vh;
+}
+
+img {
+    max-width: 100%;
+}
+
+#root, .App {
+  height: 100%;
+}
+
+
+
+`;
