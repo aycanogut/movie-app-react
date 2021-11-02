296394824856b23dc19bf190fb2609a4403de2f0
Tuba Özyurt <tuba.ozyurt93@gmail.com>
2021-11-02 17:44:12 +0300

    search component with style

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
 
+const StyledInput=styled.input`
+
+border-radius:20px;
+background-color:#313131;
+width: 400px;
+height: 46px;
+display: flex;
+justify-content: center;
+padding:10px;
+
+`;
 export default function Search() {
     return (
         <div>
-            Test
+            <StyledInput type="text"  placeholder="Search"/>
         </div>
     )
 }
