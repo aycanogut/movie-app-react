87ce63928fabc954ddb6d07da25a4a861a9d9b13
aycanogut <aycanonfire@gmail.com>
2021-11-03 18:24:49 +0300

    add header component and caret icon

--- src/components/CardContainer/CardContainer.styled.jsx
+++ src/components/CardContainer/CardContainer.styled.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import Title from "../Title/Title.styled";
 import Card from "../Card/Card.styled";
+import { CaretRight } from "react-bootstrap-icons";
 
 const StyledCardContainer = styled.section`
   display: flex;
 `;
 
+const StyledHeader = styled.header`
+  display: flex;
+  align-content: center;
+`;
+
 const CardContainer = () => {
   return (
-    <StyledCardContainer>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </StyledCardContainer>
+    <>
+      <StyledHeader>
+        <Title>New Releases</Title>
+        <CaretRight color="black" size={24} />
+      </StyledHeader>
+      <StyledCardContainer>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </StyledCardContainer>
+    </>
   );
 };
 
