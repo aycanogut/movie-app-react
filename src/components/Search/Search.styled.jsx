cb46c34b71d8cc65c64613811ffa3166dce4c2fb
Tuba Özyurt <tuba.ozyurt93@gmail.com>
2021-11-03 16:16:01 +0300

    add icon

--- src/components/Search/Search.styled.jsx
+++ src/components/Search/Search.styled.jsx
@@ -1,21 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
 import styled from "styled-components";
 
 const StyledInput=styled.input`
-
-border-radius:20px;
-background-color:#313131;
-width: 400px;
-height: 46px;
-display: flex;
-justify-content: center;
-padding:10px;
-
+    background-color:#313131;
+    width: 100%;
+    border-style: none;
+    padding-left: 10px;
 `;
-export default function Search() {
+
+
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 10px;
+  margin-left: 20px;
+  width: 30%;
+  position: fixed;
+  right: 300px;
+  top: 20px;
+  background-color:#313131;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+
+const Search = ({search})=>{
+
+        const [searchValue, setSearchValue] = useState("");
+      
+        const handleSearchInputChanges = e => {
+          setSearchValue(e.target.value);
+        };
+      
+       
     return (
         <div>
-            <StyledInput type="text"  placeholder="Search"/>
+           
+        
+        <SearchBox>
+           <SearchIcon src="src/assets/icons/search-icon.svg" />  
+           <StyledInput
+            placeholder="Search Movie"
+            value={searchValue}
+            onChange={handleSearchInputChanges}
+          />
+        </SearchBox>
         </div>
-    )
-}
+    );
+};
+
+export default Search;
