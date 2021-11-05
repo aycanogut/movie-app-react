e0382623812c2eaa1a3886b2a4fa8351ed4e1972
aycanogut <aycanonfire@gmail.com>
2021-11-05 17:08:43 +0300

    add swiper.js carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,36 +1,55 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar } from "swiper";
-import "swiper/swiper-bundle.min.css";
-import CarouselBg from "../../assets/images/carousel.png";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import SwiperCore, { Autoplay, Parallax } from "swiper";
+
+SwiperCore.use([Autoplay, Parallax]);
 
 const Carousel = () => {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar]}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      //todo look for usage of pagination and scrollbar, also add auto change of slides
-      spaceBetween={0}
-      slidesPerView={1}
-      centeredSlides
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>
-        <img src={CarouselBg} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={CarouselBg} alt="" />{" "}
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={CarouselBg} alt="" />{" "}
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={CarouselBg} alt="" />{" "}
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <Swiper
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        speed={600}
+        parallax={true}
+        slidesPerView={1}
+        centeredSlides={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2059&q=80"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
+            alt=""
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />{" "}
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 
