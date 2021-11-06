144d8dfb635a17cc0481701c4552d17af5b4d67b
aycanogut <aycanonfire@gmail.com>
2021-11-06 21:57:46 +0300

    add router and routes

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Header from "../Header/Header.styled";
+import Card from "../Card/Card.styled";
 import Wrapper from "../Wrapper/Wrapper.styled";
 
 function Page() {
   return (
-    <>
+    <Router>
       <Header />
-      <Wrapper />
-    </>
+      <Routes>
+        <Route path="/" element={<Wrapper />} />
+        <Route path="/card" element={<Card />} />
+      </Routes>
+    </Router>
   );
 }
 
