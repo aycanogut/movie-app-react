e3e76fbc40e2cd50140229f99d8218a3039b5558
aycanogut <aycanonfire@gmail.com>
2021-11-06 21:58:00 +0300

    update color dynamically

--- src/components/Title/Title.styled.jsx
+++ src/components/Title/Title.styled.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import helpers from "../helpers";
 
 const StyledTitle = styled.h4`
+  color: ${helpers.colors.text}
   font-size: 2.4em;
   font-weight: 500;
   margin: 0;
