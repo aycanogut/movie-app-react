37f4db65c8c47f60f970c2654ae301d1920ea64d
aycanogut <aycanonfire@gmail.com>
2021-11-06 21:58:18 +0300

    update wrapper children

--- src/components/Wrapper/Wrapper.styled.jsx
+++ src/components/Wrapper/Wrapper.styled.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import Homepage from "../Homepage/Homepage.styled";
 import helpers from "../helpers";
-import CardContainer from "../CardContainer/CardContainer.styled";
 
 const StyledWrapper = styled.div`
-  max-width: 300px;
+  /* max-width: 300px; */
   margin: 0 auto;
 
   @media only screen and ${helpers.device.sm} {
-    max-width: 450px;
+    /* max-width: 450px; */
   }
 
   @media only screen and ${helpers.device.md} {
-    max-width: 700px;
+    /* max-width: 700px; */
   }
 
   @media only screen and ${helpers.device.lg} {
-    max-width: 1000px;
+    /* max-width: 1000px; */
   }
 `;
 
 const Wrapper = () => {
   return (
     <StyledWrapper>
-      <CardContainer />
+      <Homepage />
     </StyledWrapper>
   );
 };
