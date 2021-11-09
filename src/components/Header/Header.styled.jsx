51a9794a5b8e0a1e849838357d04bb89018efffa
aycanogut <aycanonfire@gmail.com>
2021-11-09 19:15:58 +0300

    remove hero and input components

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import Navbar from "../Navbar/Navbar.styled";
-import Hero from "../Hero/Hero.styled";
-import SearchInput from "../Search/Search.styled";
 import Carousel from "../Carousel/Carousel";
 
-const StyledHeader = styled.header`
-  position: relative;
-`;
+const StyledHeader = styled.header``;
 
 function Header() {
   return (
     <StyledHeader>
       <Carousel />
       <Navbar />
-      <Hero />
-      <SearchInput />
     </StyledHeader>
   );
 }
