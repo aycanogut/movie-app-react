6a07d9cf2fd1cf50f9b3a8b87132b7e0964f2cbf
aycanogut <aycanonfire@gmail.com>
2021-11-09 19:15:44 +0300

    add catalog page

---
+++ src/components/Catalog/Catalog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header/Header.styled";
+import CardContainer from "../CardContainer/CardContainer.styled";
+import SearchInput from "../SearchInput/SearchInput.styled";
+
+const Catalog = () => {
+  return (
+    <>
+      <Header />
+      <SearchInput />
+      <CardContainer />
+    </>
+  );
+};
+
+export default Catalog;
