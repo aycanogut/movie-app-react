19a32445f7faabd44745c4fb473a75658b41c1ae
aycanogut <aycanonfire@gmail.com>
2021-11-09 19:15:36 +0300

    add component data dynamically, merge hero

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -10,46 +11,128 @@ import SwiperCore, { Autoplay, Parallax } from "swiper";
 
 SwiperCore.use([Autoplay, Parallax]);
 
+import helpers from "../helpers";
+import tmdbApi, { category, movieType } from "../../api/tmdbApi.js";
+import config from "../../api/config.js";
+
+// header title
+const StyledWrapper = styled.section`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: grid;
+  place-items: start;
+  height: 20%;
+  padding: 12% 4rem;
+  color: ${helpers.colors.text};
+
+  @media only screen and ${helpers.device.sm} {
+    padding: 20% 8rem;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    max-width: 500px;
+    padding: 20% 12rem;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    max-width: 600px;
+    padding: 12rem;
+    margin: 10rem;
+  }
+`;
+
+const StyledBadge = styled.span`
+  padding: 6px;
+  border-radius: 12px;
+  background-color: rgba(29, 29, 29, 0.5);
+  color: ${helpers.colors.textBadge};
+  font-size: 10px;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 12px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 14px;
+  } ;
+`;
+
+const Styledh1 = styled.h1`
+  font-size: 22px;
+  margin: 1rem 0 0 0;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 24px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 32px;
+    margin: 2rem 0 2rem 0;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    font-size: 64px;
+  } ;
+`;
+
+const StyledP = styled.p`
+  font-size: 12px;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 14px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 18px;
+    line-height: 1.5;
+  }
+`;
+
 const Carousel = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const params = { page: 1 };
+      try {
+        const response = await tmdbApi.getMoviesList(movieType.popular, {
+          params,
+        });
+        setMovies(response.results.slice(0, 5));
+        console.log(movies);
+      } catch {
+        console.log("error");
+      }
+    };
+    getMovies();
+  }, []);
+
   return (
-    <>
-      <Swiper
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        speed={600}
-        parallax={true}
-        slidesPerView={1}
-        centeredSlides={true}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img
-            src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2059&q=80"
-            alt=""
-          />
+    <Swiper
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      speed={600}
+      parallax={true}
+      grabCursor={true}
+      slidesPerView={1}
+      centeredSlides={true}
+      className="mySwiper"
+    >
+      {movies.map((movie, index) => (
+        <SwiperSlide key={index}>
+          <img src={config.images(movie.backdrop_path)} alt="" />
+          <StyledWrapper>
+            <StyledBadge></StyledBadge>
+            <Styledh1>{movie.original_title}</Styledh1>
+            <StyledP>{movie.overview.substring(0, 220)}</StyledP>
+          </StyledWrapper>
         </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
-            alt=""
-          />
-        </SwiperSlide>
-      </Swiper>
-    </>
+      ))}
+    </Swiper>
   );
 };
 
