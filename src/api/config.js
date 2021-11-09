f1d2cf42a5c923dbcb79457df11584aa8b0c6a07
aycanogut <aycanonfire@gmail.com>
2021-11-09 19:14:54 +0300

    add api call and configs

---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+const config = {
+  url: "https://api.themoviedb.org/3/",
+  key: "12a8a34f7770ec8f55d92a5f438fc87e",
+  images: (path) => `https://image.tmdb.org/t/p/original/${path}`,
+  w500images: (path) => `https://image.tmdb.org/t/p/w500/${path}`,
+};
+
+export default config;
