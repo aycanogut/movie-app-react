a934eb247d832886f0e93fc2fbb001970749a49a
aycanogut <aycanonfire@gmail.com>
2021-11-10 16:33:17 +0300

    update responsive behaviour

--- src/components/Title/Title.styled.jsx
+++ src/components/Title/Title.styled.jsx
@@ -1,16 +1,40 @@
-import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import helpers from "../helpers";
 
-const StyledTitle = styled.h4`
-  color: ${helpers.colors.text}
-  font-size: 2.4em;
-  font-weight: 500;
-  margin: 0;
+const StyledTitle = styled.header`
+  color: ${helpers.colors.text};
+  font-size: 1.6rem;
+  margin: 0 0 0 1.4rem;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 1.8rem;
+    margin: 0 0 0 1.8rem;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 2.4rem;
+    margin: 0 0 0 2.2rem;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    font-size: 3rem;
+    margin: 0 0 0 4rem;
+  }
 `;
 
-const Title = ({ children }) => {
-  return <StyledTitle>{children}</StyledTitle>;
+import React from "react";
+
+const Title = ({ title }) => {
+  return (
+    <StyledTitle>
+      <p>{title}</p>
+    </StyledTitle>
+  );
+};
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Title;
