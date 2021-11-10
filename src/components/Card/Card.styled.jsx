8e58f07e610a0a6fc7acba74df527e0b74c46369
aycanogut <aycanonfire@gmail.com>
2021-11-10 15:52:46 +0300

    remove styled stuff

--- src/components/Card/Card.styled.jsx
+++ src/components/Card/Card.styled.jsx
@@ -1,32 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-import helpers from "../helpers";
 
-const StyledCard = styled.article`
-  /* position: relative; */
-`;
-
-const StyledImg = styled.img`
-  /* css */
-`;
-
-const StyledTitle = styled.header`
-  position: absolute;
-  left: 24px;
-  bottom: 24px;
-  color: ${helpers.colors.text};
-
-  font-size: 24px;
-`;
-
-const Card = ({ image, title }) => {
-  return (
-    <StyledCard>
-      <StyledImg src={image} />
-      <StyledTitle>{title}</StyledTitle>
-    </StyledCard>
-  );
+const Card = ({ image }) => {
+  return <img src={image} />;
 };
 
 Card.propTypes = {
