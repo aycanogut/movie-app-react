67a32342b3a0d53c84ff8ac9c12f418c16a38d29
aycanogut <aycanonfire@gmail.com>
2021-11-10 16:32:48 +0300

    add margin bottom

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 import Navbar from "../Navbar/Navbar.styled";
 import Carousel from "../Carousel/Carousel";
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  margin: 0 0 4.5rem 0;
+`;
 
 function Header() {
   return (
