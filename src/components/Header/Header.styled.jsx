f4e80ddc24c4c93ec70cb51bb078a69900b03e5e
aycanogut <aycanonfire@gmail.com>
2021-11-10 17:45:29 +0300

    update header styles

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -4,7 +4,9 @@ import Navbar from "../Navbar/Navbar.styled";
 import Carousel from "../Carousel/Carousel";
 
 const StyledHeader = styled.header`
-  margin: 0 0 4.5rem 0;
+  margin: 0 auto 4.5rem auto;
+  max-width: 1920px;
+  max-height: 1080px;
 `;
 
 function Header() {
