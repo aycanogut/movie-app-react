9048a1422d8dde6c39956ebeff8a48f1cf6984a3
aycanogut <aycanonfire@gmail.com>
2021-11-10 14:35:23 +0300

    moved Hero component to another file

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -11,84 +10,10 @@ import SwiperCore, { Autoplay, Parallax } from "swiper";
 
 SwiperCore.use([Autoplay, Parallax]);
 
-import helpers from "../helpers";
+// api config
 import tmdbApi, { category, movieType } from "../../api/tmdbApi.js";
 import config from "../../api/config.js";
-
-// header title
-const StyledWrapper = styled.section`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  display: grid;
-  place-items: start;
-  height: 20%;
-  padding: 12% 4rem;
-  color: ${helpers.colors.text};
-
-  @media only screen and ${helpers.device.sm} {
-    padding: 20% 8rem;
-  }
-
-  @media only screen and ${helpers.device.md} {
-    max-width: 500px;
-    padding: 20% 12rem;
-  }
-
-  @media only screen and ${helpers.device.lg} {
-    max-width: 600px;
-    padding: 12rem;
-    margin: 10rem;
-  }
-`;
-
-const StyledBadge = styled.span`
-  padding: 6px;
-  border-radius: 12px;
-  background-color: rgba(29, 29, 29, 0.5);
-  color: ${helpers.colors.textBadge};
-  font-size: 10px;
-
-  @media only screen and ${helpers.device.sm} {
-    font-size: 12px;
-  }
-
-  @media only screen and ${helpers.device.md} {
-    font-size: 14px;
-  } ;
-`;
-
-const Styledh1 = styled.h1`
-  font-size: 22px;
-  margin: 1rem 0 0 0;
-
-  @media only screen and ${helpers.device.sm} {
-    font-size: 24px;
-  }
-
-  @media only screen and ${helpers.device.md} {
-    font-size: 32px;
-    margin: 2rem 0 2rem 0;
-  }
-
-  @media only screen and ${helpers.device.lg} {
-    font-size: 64px;
-  } ;
-`;
-
-const StyledP = styled.p`
-  font-size: 12px;
-
-  @media only screen and ${helpers.device.sm} {
-    font-size: 14px;
-  }
-
-  @media only screen and ${helpers.device.md} {
-    font-size: 18px;
-    line-height: 1.5;
-  }
-`;
+import Hero from "../Hero/Hero.styled";
 
 const Carousel = () => {
   const [movies, setMovies] = useState([]);
@@ -125,11 +50,11 @@ const Carousel = () => {
       {movies.map((movie, index) => (
         <SwiperSlide key={index}>
           <img src={config.images(movie.backdrop_path)} alt="" />
-          <StyledWrapper>
-            <StyledBadge></StyledBadge>
-            <Styledh1>{movie.original_title}</Styledh1>
-            <StyledP>{movie.overview.substring(0, 220)}</StyledP>
-          </StyledWrapper>
+          <Hero
+            badge={movie.original_title}
+            title={movie.original_title}
+            text={movie.overview.substring(0, 220)}
+          />
         </SwiperSlide>
       ))}
     </Swiper>
---
+++ src/components/Hero/Hero.styled.jsx
@@ -0,0 +1,95 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import helpers from "../helpers";
+
+// header title
+export const StyledHero = styled.section`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: grid;
+  place-items: start;
+  height: 20%;
+  padding: 12% 4rem;
+  color: ${helpers.colors.text};
+
+  @media only screen and ${helpers.device.sm} {
+    padding: 20% 8rem;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    max-width: 500px;
+    padding: 20% 12rem;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    max-width: 600px;
+    padding: 12rem;
+    margin: 10rem;
+  }
+`;
+export const StyledBadge = styled.span`
+  padding: 6px;
+  border-radius: 12px;
+  background-color: rgba(29, 29, 29, 0.5);
+  color: ${helpers.colors.textBadge};
+  font-size: 10px;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 12px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 14px;
+  } ;
+`;
+export const StyledTitle = styled.h1`
+  font-size: 22px;
+  margin: 1rem 0 0 0;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 24px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 32px;
+    margin: 2rem 0 2rem 0;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    font-size: 64px;
+  } ;
+`;
+export const StyledText = styled.p`
+  font-size: 12px;
+
+  @media only screen and ${helpers.device.sm} {
+    font-size: 14px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 18px;
+    line-height: 1.5;
+  }
+`;
+
+import React from "react";
+
+function Hero({ badge, title, text }) {
+  return (
+    <StyledHero>
+      <StyledBadge>{badge}</StyledBadge>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledText>{text}</StyledText>
+    </StyledHero>
+  );
+}
+
+Hero.propTypes = {
+  badge: PropTypes.string,
+  title: PropTypes.string,
+  text: PropTypes.string,
+};
+
+export default Hero;
