57a9606a59c094b2178aad0e5f32e4eebb8d953a
aycanogut <aycanonfire@gmail.com>
2021-11-10 16:33:03 +0300

    add tv shows stuff

--- src/components/Homepage/Homepage.styled.jsx
+++ src/components/Homepage/Homepage.styled.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-import helpers from "../helpers";
 import Header from "../Header/Header.styled";
 import Title from "../Title/Title.styled";
 import Card from "../Card/Card.styled";
@@ -10,27 +8,26 @@ import Card from "../Card/Card.styled";
 import "swiper/css";
 
 // api config
-import tmdbApi, { category, movieType } from "../../api/tmdbApi.js";
+import tmdbApi, { movieType, tvType } from "../../api/tmdbApi.js";
 import config from "../../api/config.js";
-
-const StyledHeader = styled.header`
-  display: flex;
-  align-content: center;
-  margin-block: 4rem;
-  color: ${helpers.colors.text};
-`;
 
 const Homepage = () => {
   const [movies, setMovies] = useState([]);
+  const [tvShows, setTvShows] = useState([]);
 
   useEffect(() => {
     const getMovies = async () => {
       const params = { page: 1 };
       try {
-        const response = await tmdbApi.getMoviesList(movieType.popular, {
+        const responseMovies = await tmdbApi.getMoviesList(movieType.popular, {
           params,
         });
-        setMovies(response.results.slice(0, 20));
+        const responseTv = await tmdbApi.getTvList(tvType.popular, {
+          params,
+        });
+
+        setMovies(responseMovies.results.slice(0, 20));
+        setTvShows(responseTv.results.slice(0, 20));
       } catch {
         console.log("error");
       }
@@ -41,9 +38,7 @@ const Homepage = () => {
   return (
     <>
       <Header />
-      <StyledHeader>
-        <Title>New Releases</Title>
-      </StyledHeader>
+      <Title title={"New Releases"} />
       <Swiper
         grabCursor={true}
         autoplay={{
@@ -74,6 +69,7 @@ const Homepage = () => {
             slidesPerView: 3,
           },
         }}
+        style={{ marginBlock: "5rem" }}
       >
         {movies.map((movie, index) => (
           <SwiperSlide key={index}>
@@ -81,7 +77,50 @@ const Homepage = () => {
               image={config.w500images(movie.poster_path)}
               title={movie.original_title}
             />
-            {console.log(movie)}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Title title={"Featured TV Shows"} />
+
+      <Swiper
+        grabCursor={true}
+        autoplay={{
+          delay: 9999999999999,
+          disableOnInteraction: false,
+        }}
+        className="mySwiper"
+        breakpoints={{
+          320: {
+            width: 160,
+            slidesPerView: 1,
+          },
+          465: {
+            width: 320,
+            slidesPerView: 2,
+          },
+          640: {
+            width: 500,
+            slidesPerView: 2,
+          },
+
+          768: {
+            width: 700,
+            slidesPerView: 3,
+          },
+          992: {
+            width: 992,
+            slidesPerView: 3,
+          },
+        }}
+        style={{ marginBlock: "5rem" }}
+      >
+        {tvShows.map((tv, index) => (
+          <SwiperSlide key={index}>
+            <Card
+              image={config.w500images(tv.poster_path)}
+              title={tv.original_title}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
