bc3c87caf26e3a93496d969ce348307b90207664
aycanogut <aycanonfire@gmail.com>
2021-11-10 15:40:45 +0300

    add data dynamically

--- src/components/Homepage/Homepage.styled.jsx
+++ src/components/Homepage/Homepage.styled.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import helpers from "../helpers";
 import Header from "../Header/Header.styled";
+import Title from "../Title/Title.styled";
+import Card from "../Card/Card.styled";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 
-import SwiperCore, { Autoplay, Parallax } from "swiper";
-import Title from "../Title/Title.styled";
-
-SwiperCore.use([Autoplay, Parallax]);
+// api config
+import tmdbApi, { category, movieType } from "../../api/tmdbApi.js";
+import config from "../../api/config.js";
 
 const StyledHeader = styled.header`
   display: flex;
@@ -22,6 +21,23 @@ const StyledHeader = styled.header`
 `;
 
 const Homepage = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const params = { page: 1 };
+      try {
+        const response = await tmdbApi.getMoviesList(movieType.popular, {
+          params,
+        });
+        setMovies(response.results.slice(0, 20));
+      } catch {
+        console.log("error");
+      }
+    };
+    getMovies();
+  }, []);
+
   return (
     <>
       <Header />
@@ -29,28 +45,46 @@ const Homepage = () => {
         <Title>New Releases</Title>
       </StyledHeader>
       <Swiper
+        grabCursor={true}
+        slidesPerView={4}
         autoplay={{
-          delay: 2500,
+          delay: 9999999999999,
           disableOnInteraction: false,
         }}
-        speed={600}
-        parallax={true}
-        slidesPerView={2}
-        centeredSlides={false}
+        className="mySwiper"
         breakpoints={{
-          // when window width is >= 640px
+          320: {
+            width: 320,
+            slidesPerView: 1,
+          },
+          465: {
+            width: 465,
+            slidesPerView: 2,
+          },
           640: {
             width: 640,
-            slidesPerView: 3,
+            slidesPerView: 2,
           },
-          // when window width is >= 768px
+
           768: {
             width: 768,
-            slidesPerView: 4,
+            slidesPerView: 3,
+          },
+          992: {
+            width: 992,
+            slidesPerView: 2,
           },
         }}
-        className="mySwiper"
-      ></Swiper>
+      >
+        {movies.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <Card
+              image={config.w500images(movie.backdrop_path)}
+              title={movie.original_title}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
