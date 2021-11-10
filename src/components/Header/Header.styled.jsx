d7c977dd6660ff6806f574927e0100543d11cca5
aycanogut <aycanonfire@gmail.com>
2021-11-10 18:02:03 +0300

    add spacing to header

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import helpers from "../helpers";
 import Navbar from "../Navbar/Navbar.styled";
 import Carousel from "../Carousel/Carousel";
 
 const StyledHeader = styled.header`
-  margin: 0 auto 4.5rem auto;
+  margin: 6rem auto 0 auto;
   max-width: 1920px;
   max-height: 1080px;
+
+  @media only screen and ${helpers.device.lg} {
+    margin: 0 auto;
+  }
 `;
 
 function Header() {
