13ae51f50a754bb49d6368b7ae6e26f5a5c565ba
aycanogut <aycanonfire@gmail.com>
2021-11-12 00:09:06 +0300

    add wrapper and hero carousel

--- src/components/Homepage/Homepage.styled.jsx
+++ src/components/Homepage/Homepage.styled.jsx
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Header from "../Header/Header.styled";
+import { SwiperSlide } from "swiper/react";
+import helpers from "../helpers";
+import Navbar from "../Navbar/Navbar.styled";
 import Title from "../Title/Title.styled";
 import Card from "../Card/Card.styled";
-
-// Import Swiper styles
-import "swiper/css";
+import Carousel from "../Carousel/Carousel";
+import Hero from "../Hero/Hero.styled";
 
 // api config
 import tmdbApi, { movieType, tvType } from "../../api/tmdbApi.js";
 import config from "../../api/config.js";
 
 const StyledWrapper = styled.div`
+  margin: 6rem auto 0 auto;
   max-width: 1920px;
-  margin: 0 auto;
+  max-height: 1080px;
+
+  @media only screen and ${helpers.device.lg} {
+    margin: 0 auto;
+  }
 `;
 
 const Homepage = () => {
   const [movies, setMovies] = useState([]);
   const [tvShows, setTvShows] = useState([]);
+  const heroMovies = movies.slice(0, 5);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -42,101 +48,101 @@ const Homepage = () => {
   }, []);
 
   return (
-    <>
-      <Header />
-      <StyledWrapper>
-        <Title title={"New Releases"} />
-        <Swiper
-          grabCursor={true}
-          autoplay={{
-            delay: 9999999999999,
-            disableOnInteraction: false,
-          }}
-          className="mySwiper"
-          breakpoints={{
-            320: {
-              width: 160,
-              slidesPerView: 1,
-            },
-            465: {
-              width: 320,
-              slidesPerView: 2,
-            },
-            640: {
-              width: 500,
-              slidesPerView: 2,
-            },
+    <StyledWrapper>
+      <Navbar />
+      <Carousel>
+        {heroMovies.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={config.images(movie.backdrop_path)}
+              alt={`poster of ${movie.original_title} movie`}
+            />
+            <Hero
+              title={movie.original_title}
+              text={movie.overview.substring(0, 220)}
+            />
+          </SwiperSlide>
+        ))}
+      </Carousel>
+      {/* tv shows section start */}
+      <Title title={"New Releases"} />
+      <Carousel
+        breakpoints={{
+          320: {
+            width: 160,
+            slidesPerView: 1,
+          },
+          465: {
+            width: 320,
+            slidesPerView: 2,
+          },
+          640: {
+            width: 500,
+            slidesPerView: 2,
+          },
 
-            768: {
-              width: 700,
-              slidesPerView: 3,
-            },
-            992: {
-              width: 992,
-              slidesPerView: 3,
-            },
-          }}
-        >
-          {movies.map((movie, index) => (
-            <SwiperSlide key={index}>
-              <Card
-                image={config.w500images(movie.poster_path)}
-                title={movie.original_title}
-                rating={movie.vote_average}
-                info={movie.overview.slice(0, 180).concat("...")}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+          768: {
+            width: 700,
+            slidesPerView: 3,
+          },
+          992: {
+            width: 992,
+            slidesPerView: 3,
+          },
+        }}
+      >
+        {movies.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <Card
+              image={config.w500images(movie.poster_path)}
+              title={movie.original_title}
+              rating={movie.vote_average}
+              info={movie.overview.slice(0, 180).concat("...")}
+            />
+          </SwiperSlide>
+        ))}
+      </Carousel>
+      {/* tv shows section end */}
+      {/* movies section start */}
+      <Title title={"Featured TV Shows"} />
+      <Carousel
+        breakpoints={{
+          320: {
+            width: 160,
+            slidesPerView: 1,
+          },
+          465: {
+            width: 320,
+            slidesPerView: 2,
+          },
+          640: {
+            width: 500,
+            slidesPerView: 2,
+          },
 
-        <Title title={"Featured TV Shows"} />
-
-        <Swiper
-          grabCursor={true}
-          autoplay={{
-            delay: 9999999999999,
-            disableOnInteraction: false,
-          }}
-          className="mySwiper"
-          breakpoints={{
-            320: {
-              width: 160,
-              slidesPerView: 1,
-            },
-            465: {
-              width: 320,
-              slidesPerView: 2,
-            },
-            640: {
-              width: 500,
-              slidesPerView: 2,
-            },
-
-            768: {
-              width: 700,
-              slidesPerView: 3,
-            },
-            992: {
-              width: 992,
-              slidesPerView: 3,
-            },
-          }}
-        >
-          {tvShows.map((tv, index) => (
-            <SwiperSlide key={index}>
-              <Card
-                image={config.w500images(tv.poster_path)}
-                title={tv.original_name}
-                rating={tv.vote_average}
-                genre={tv.genre_ids}
-                info={tv.overview.slice(0, 180).concat("...")}
-              />
-              {console.log(tv)}
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </StyledWrapper>
-    </>
+          768: {
+            width: 700,
+            slidesPerView: 3,
+          },
+          992: {
+            width: 992,
+            slidesPerView: 3,
+          },
+        }}
+      >
+        {tvShows.map((tv, index) => (
+          <SwiperSlide key={index}>
+            <Card
+              image={config.w500images(tv.poster_path)}
+              title={tv.original_name}
+              rating={tv.vote_average}
+              info={tv.overview.slice(0, 180).concat("...")}
+            />
+          </SwiperSlide>
+        ))}
+      </Carousel>
+      {/* movies section start */}
+    </StyledWrapper>
   );
 };
 
