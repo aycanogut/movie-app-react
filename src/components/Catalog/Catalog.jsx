deddff5f11aa9b098598747e9a7811577213de9e
aycanogut <aycanonfire@gmail.com>
2021-11-12 00:25:02 +0300

    add carousel to the catalog page

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,12 +1,51 @@
-import React from "react";
-import Header from "../Header/Header.styled";
+import React, { useState, useEffect } from "react";
+import { SwiperSlide } from "swiper/react";
 import CardContainer from "../CardContainer/CardContainer.styled";
+import Carousel from "../Carousel/Carousel";
+import Hero from "../Hero/Hero.styled";
+import Navbar from "../Navbar/Navbar.styled";
 import SearchInput from "../SearchInput/SearchInput.styled";
 
+// api config
+import tmdbApi, { movieType } from "../../api/tmdbApi.js";
+import config from "../../api/config.js";
+
 const Catalog = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const params = { page: 1 };
+      try {
+        const response = await tmdbApi.getMoviesList(movieType.popular, {
+          params,
+        });
+
+        setMovies(response.results.slice(0, 5));
+      } catch {
+        console.log("error");
+      }
+    };
+    getMovies();
+  }, []);
+
   return (
     <>
-      <Header />
+      <Navbar />
+      <Carousel>
+        {movies.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={config.images(movie.backdrop_path)}
+              alt={`poster of ${movie.original_title} movie`}
+            />
+            <Hero
+              title={movie.original_title}
+              text={movie.overview.substring(0, 220)}
+            />
+          </SwiperSlide>
+        ))}
+      </Carousel>
       <SearchInput />
       <CardContainer />
     </>
