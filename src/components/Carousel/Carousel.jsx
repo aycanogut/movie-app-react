757cdf95b7db89b1d60463b010808196e2030a33
aycanogut <aycanonfire@gmail.com>
2021-11-12 00:08:48 +0300

    remove api config and move to homepage

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Hero from "../Hero/Hero.styled";
+import React from "react";
+import { Swiper } from "swiper/react";
+import PropTypes from "prop-types";
 
 // Import Swiper styles
 import "swiper/css";
@@ -11,55 +11,25 @@ import SwiperCore, { Autoplay, Parallax } from "swiper";
 
 SwiperCore.use([Autoplay, Parallax]);
 
-// api config
-import tmdbApi, { category, movieType } from "../../api/tmdbApi.js";
-import config from "../../api/config.js";
-
-const Carousel = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const params = { page: 1 };
-      try {
-        const response = await tmdbApi.getMoviesList(movieType.popular, {
-          params,
-        });
-        setMovies(response.results.slice(0, 5));
-      } catch {
-        console.log("error");
-      }
-    };
-    getMovies();
-  }, []);
-
+const Carousel = ({ children, breakpoints }) => {
   return (
     <Swiper
+      grabCursor={true}
       autoplay={{
-        delay: 5500,
+        delay: 9999999999999,
         disableOnInteraction: false,
       }}
-      speed={600}
-      parallax={true}
-      grabCursor={true}
-      slidesPerView={1}
-      centeredSlides={true}
       className="mySwiper"
+      breakpoints={breakpoints}
     >
-      {movies.map((movie, index) => (
-        <SwiperSlide key={index}>
-          <img
-            src={config.images(movie.backdrop_path)}
-            alt={`poster of ${movie.original_title} movie`}
-          />
-          <Hero
-            title={movie.original_title}
-            text={movie.overview.substring(0, 220)}
-          />
-        </SwiperSlide>
-      ))}
+      {children}
     </Swiper>
   );
+};
+
+Carousel.propTypes = {
+  children: PropTypes.node.isRequired,
+  breakpoints: PropTypes.object,
 };
 
 export default Carousel;
