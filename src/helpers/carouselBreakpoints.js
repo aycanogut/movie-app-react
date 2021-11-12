d91e71155f0f3659529ef4d603cec0a651a91ce7
aycanogut <aycanonfire@gmail.com>
2021-11-12 17:02:18 +0300

    add breakpoints object

---
+++ src/helpers/carouselBreakpoints.js
@@ -0,0 +1,25 @@
+const breakpoints = {
+  320: {
+    width: 160,
+    slidesPerView: 1,
+  },
+  465: {
+    width: 320,
+    slidesPerView: 2,
+  },
+  640: {
+    width: 500,
+    slidesPerView: 2,
+  },
+
+  768: {
+    width: 700,
+    slidesPerView: 3,
+  },
+  992: {
+    width: 992,
+    slidesPerView: 3,
+  },
+};
+
+export default breakpoints;
