8f63d0c4f2dc1a6b29ee34092596b55b20e77e72
aycanogut <aycanonfire@gmail.com>
2021-11-16 00:09:43 +0300

    add comments and if statement

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
+// configuration for themoviedb api
 export const api = {
   baseUrl: "https://api.themoviedb.org/3/",
   key: "12a8a34f7770ec8f55d92a5f438fc87e",
@@ -8,33 +9,42 @@ export const api = {
   w500images: (path) => `https://image.tmdb.org/t/p/w500/${path}`,
 };
 
+// url configuration
 export const url = {
-  search: (movie) =>
-    `${api.baseUrl}search/multi?api_key=${api.key}&query=${movie}`,
+  search: (item) =>
+    `${api.baseUrl}search/multi?api_key=${api.key}&query=${item}`,
   movies: () => `${api.baseUrl}movie/popular?api_key=${api.key}&language=en-US`,
   series: () => `${api.baseUrl}tv/popular?api_key=${api.key}&language=en-US`,
 };
 
+// custom hook for fetch data from api
 const useData = () => {
   const [movies, setMovies] = useState([]);
   const [series, setSeries] = useState([]);
+  const [search, setSearch] = useState([]);
 
-  // movies
-  const getMovies = async () => {
-    const { data } = await axios(url.movies());
+  // this function is using in the homepage and catalog pages for fetching data from api
+  const submitRequest = async (link, setState) => {
+    const { data } = await axios(link);
     const { results } = data;
 
-    setMovies(results);
+    if (!data) {
+      console.log("error");
+      return;
+    }
+
+    setState(results);
   };
 
-  const getSeries = async () => {
-    const { data } = await axios(url.series());
-    const { results } = data;
-
-    setSeries(results);
+  return {
+    movies,
+    series,
+    search,
+    setMovies,
+    setSeries,
+    setSearch,
+    submitRequest,
   };
-
-  return { movies, series, getMovies, getSeries };
 };
 
 export default useData;
