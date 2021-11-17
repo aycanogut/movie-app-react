3c76592db3e1ff19b59a894852f57fe8dec7f02b
aycanogut <aycanonfire@gmail.com>
2021-11-17 18:48:07 +0300

    update poster image width

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const api = {
   baseUrl: "https://api.themoviedb.org/3/",
   key: "12a8a34f7770ec8f55d92a5f438fc87e",
   images: (path) => `https://image.tmdb.org/t/p/original/${path}`,
-  w500images: (path) => `https://image.tmdb.org/t/p/w500/${path}`,
+  w500images: (path) => `https://image.tmdb.org/t/p/w342/${path}`,
 };
 
 export default api;
