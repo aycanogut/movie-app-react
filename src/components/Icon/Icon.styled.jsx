de26438c72688622ef1ec20eab9b0bb644c82638
aycanogut <aycanonfire@gmail.com>
2021-11-17 20:46:03 +0300

    add responsive icon styles

--- src/components/Icon/Icon.styled.jsx
+++ src/components/Icon/Icon.styled.jsx
@@ -1,8 +1,20 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import helpers from "../helpers";
 
 const StyledIcon = styled.img`
-  /* css */
+  width: 2.2rem;
+  height: 2.2rem;
+
+  @media only screen and ${helpers.device.md} {
+    width: 2.4rem;
+    height: 2.4rem;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    width: 3rem;
+    height: 3rem;
+  } ;
 `;
 
 function Icon({ source }) {
