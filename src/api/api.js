1974ec8663f086fdac07d0527ff3689c4db174ca
aycanogut <aycanonfire@gmail.com>
2021-11-17 21:22:39 +0300

    update image resolution

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@
 const api = {
   baseUrl: "https://api.themoviedb.org/3/",
   key: "12a8a34f7770ec8f55d92a5f438fc87e",
-  images: (path) => `https://image.tmdb.org/t/p/original/${path}`,
+  images: (path) => `https://image.tmdb.org/t/p/w1280/${path}`,
   w500images: (path) => `https://image.tmdb.org/t/p/w342/${path}`,
 };
 
