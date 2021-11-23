a3e15843121ebc10677d81c186743caf436a1346
aycanogut <aycanonfire@gmail.com>
2021-11-23 19:27:27 +0300

    update imports and dependency array

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { SwiperSlide } from "swiper/react";
 
+import useFavorites from "../../hooks/useFavorites";
 import useData, { url } from "../../hooks/useData";
 import api from "../../api/api.js";
 import helpers from "../helpers";
-import carouselBreakpoints from "../../helpers/carouselBreakpoints";
 
 import Navbar from "../Navbar/Navbar.styled";
-import Title from "../Title/Title.styled";
-import Card from "../Card/Card.styled";
-import Carousel from "../Carousel/Carousel";
 import Hero from "../Hero/Hero.styled";
+import Carousel from "../Carousel/Carousel";
+import CardContainer from "../CardContainer/CardContainer.styled";
+import Title from "../Title/Title.styled";
 
 const StyledWrapper = styled.div`
   margin: 6rem auto 0 auto;
@@ -29,6 +29,7 @@ const StyledImage = styled.img`
 const Favorites = () => {
   const { submitRequest } = useData();
 
+  const { favorites, addFavorite, removeFavorite } = useFavorites();
   const [heroImage, setHeroImage] = useState([]);
 
   const fetchData = async (name, location, setState) => {
@@ -38,7 +39,7 @@ const Favorites = () => {
 
   useEffect(() => {
     fetchData(heroImage, url.popularMovies(), setHeroImage);
-  }, []);
+  }, [favorites]);
 
   return (
     <StyledWrapper>
@@ -58,6 +59,7 @@ const Favorites = () => {
         ))}
       </Carousel>
       <Title title={"Favorites"} />
+      <CardContainer cardContainer={heroImage} />
     </StyledWrapper>
   );
 };
