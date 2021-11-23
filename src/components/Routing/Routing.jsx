348815cdcaedf1e2f81e73909d6b36ca4f0abc83
aycanogut <aycanonfire@gmail.com>
2021-11-23 17:06:21 +0300

    add favorites

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Loader from "../Loader/Loader";
 
-const Homepage = lazy(() => import("../Homepage/Homepage.styled"));
+const Homepage = lazy(() => import("../Homepage/Homepage"));
 const Catalog = lazy(() => import("../Catalog/Catalog"));
+const Favorites = lazy(() => import("../Favorites/Favorites"));
 
 function Routing() {
   return (
@@ -13,6 +14,7 @@ function Routing() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/search" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </Suspense>
     </Router>
