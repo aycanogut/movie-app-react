6f8003446bf11979f6e70b250892e54995c3a46d
aycanogut <aycanonfire@gmail.com>
2021-11-23 17:06:29 +0300

    create favorites component

---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { SwiperSlide } from "swiper/react";
+
+import useData, { url } from "../../hooks/useData";
+import api from "../../api/api.js";
+import helpers from "../helpers";
+import carouselBreakpoints from "../../helpers/carouselBreakpoints";
+
+import Navbar from "../Navbar/Navbar.styled";
+import Title from "../Title/Title.styled";
+import Card from "../Card/Card.styled";
+import Carousel from "../Carousel/Carousel";
+import Hero from "../Hero/Hero.styled";
+
+const StyledWrapper = styled.div`
+  margin: 6rem auto 0 auto;
+
+  @media only screen and ${helpers.device.lg} {
+    margin: 0 auto;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 1920px;
+  height: auto;
+`;
+
+const Favorites = () => {
+  const { submitRequest } = useData();
+
+  const [heroImage, setHeroImage] = useState([]);
+
+  const fetchData = async (name, location, setState) => {
+    name = await submitRequest(location);
+    setState(name);
+  };
+
+  useEffect(() => {
+    fetchData(heroImage, url.popularMovies(), setHeroImage);
+  }, []);
+
+  return (
+    <StyledWrapper>
+      <Navbar />
+      <Carousel>
+        {heroImage.map((movie, index) => (
+          <SwiperSlide key={index}>
+            <StyledImage
+              src={api.images(movie.backdrop_path)}
+              alt={`poster of ${movie.original_title} movie`}
+            />
+            <Hero
+              title={movie.original_title}
+              text={movie.overview.substring(0, 220)}
+            />
+          </SwiperSlide>
+        ))}
+      </Carousel>
+      <Title title={"Favorites"} />
+    </StyledWrapper>
+  );
+};
+
+export default Favorites;
