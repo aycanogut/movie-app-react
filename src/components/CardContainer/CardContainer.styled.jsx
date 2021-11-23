50794082b2c87d6bdbdc2b413b5d6e5235d60c35
aycanogut <aycanonfire@gmail.com>
2021-11-23 19:28:07 +0300

    remove header

--- src/components/CardContainer/CardContainer.styled.jsx
+++ src/components/CardContainer/CardContainer.styled.jsx
@@ -6,7 +6,6 @@ import api from "../../api/api.js";
 import helpers from "../helpers";
 import fallbackImage from "../../assets/images/fallback-card-image.png";
 
-import Title from "../Title/Title.styled";
 import Card from "../Card/Card.styled";
 
 const StyledCardContainer = styled.section`
@@ -34,52 +33,40 @@ const StyledCardContainer = styled.section`
   }
 `;
 
-const StyledHeader = styled.header`
-  display: flex;
-  align-content: center;
-  margin: 1rem;
-  color: ${helpers.colors.text};
-`;
-
 const CardContainer = ({ cardContainer }) => {
   return (
-    <>
-      <StyledHeader>
-        <Title title={"Results"} />
-      </StyledHeader>
-      <StyledCardContainer>
-        {cardContainer.map((item, index) => (
-          <Card
-            key={index}
-            image={
-              item.poster_path === null || item.poster_path === undefined
-                ? fallbackImage
-                : api.w500images(item.poster_path)
-            }
-            title={
-              item.original_title || item.original_name
-                ? item.original_title || item.original_name
-                : "No title"
-            }
-            rating={
-              item.vote_average === 0 ||
-              item.vote_average === undefined ||
-              !item.vote_average
-                ? "None"
-                : item.vote_average
-            }
-            info={
-              item.overview === null ||
-              item.overview === undefined ||
-              !item.overview
-                ? `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Di
+    <StyledCardContainer>
+      {cardContainer.map((item, index) => (
+        <Card
+          key={index}
+          image={
+            item.poster_path === null || item.poster_path === undefined
+              ? fallbackImage
+              : api.w500images(item.poster_path)
+          }
+          title={
+            item.original_title || item.original_name
+              ? item.original_title || item.original_name
+              : "No title"
+          }
+          rating={
+            item.vote_average === 0 ||
+            item.vote_average === undefined ||
+            !item.vote_average
+              ? "None"
+              : item.vote_average
+          }
+          info={
+            item.overview === null ||
+            item.overview === undefined ||
+            !item.overview
+              ? `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Di
                 stinctio necessitatibus debitis esse aliquid.`
-                : item.overview.slice(0, 100)
-            }
-          />
-        ))}
-      </StyledCardContainer>
-    </>
+              : item.overview.slice(0, 100)
+          }
+        />
+      ))}
+    </StyledCardContainer>
   );
 };
 
