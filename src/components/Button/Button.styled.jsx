5f3ca5c23ae7b63384683232def3ea0a4e000b7b
aycanogut <aycanonfire@gmail.com>
2021-11-25 17:33:24 +0300

    add hover effect

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -4,9 +4,20 @@ import PropTypes from "prop-types";
 import helpers from "../helpers";
 
 const StyledButton = styled.button`
-  font-size: 0.8rem;
+  transition: 0.3s ease-in-out;
   padding: 0;
   margin: 0;
+  font-size: 0.8rem;
+  background-color: transparent;
+  color: ${helpers.colors.text};
+  border: 1px solid ${helpers.colors.text};
+  cursor: pointer;
+
+  &:hover {
+    transition: 0.3s ease-in-out;
+    border: 1px solid #ffcc11;
+    color: #ffcc11;
+  }
 
   @media only screen and ${helpers.device.sm} {
     font-size: 1rem;
