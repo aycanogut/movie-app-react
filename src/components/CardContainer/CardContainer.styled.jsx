9f7e1d7fbce15503034f067564b167f595ee692c
aycanogut <aycanonfire@gmail.com>
2021-11-25 15:44:01 +0300

    add wrapper to the cards

--- src/components/CardContainer/CardContainer.styled.jsx
+++ src/components/CardContainer/CardContainer.styled.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-import api from "../../api/api.js";
+import useFavorites from "../../hooks/useFavorites";
+import api from "../../api/api";
 import helpers from "../helpers";
 import fallbackImage from "../../assets/images/fallback-card-image.png";
 
 import Card from "../Card/Card.styled";
+import Button from "../Button/Button.styled";
 
 const StyledCardContainer = styled.section`
   display: grid;
@@ -33,38 +35,75 @@ const StyledCardContainer = styled.section`
   }
 `;
 
+const StyledCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledFavorites = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 1rem 0;
+`;
+
 const CardContainer = ({ cardContainer }) => {
+  const { favorites, setFavorites, addFavorite, removeFavorite } =
+    useFavorites();
+
+  useEffect(() => {
+    const localFavorites = localStorage.getItem("favorites");
+    const parsedFavorites = JSON.parse(localFavorites);
+
+    parsedFavorites ? setFavorites(parsedFavorites) : setFavorites([]);
+  }, []);
+
+  useEffect(() => {
+    const localFavorites = JSON.stringify(favorites);
+    localStorage.setItem("favorites", localFavorites);
+  }, [favorites]);
+
   return (
     <StyledCardContainer>
       {cardContainer.map((item, index) => (
-        <Card
-          key={index}
-          image={
-            item.poster_path === null || item.poster_path === undefined
-              ? fallbackImage
-              : api.w500images(item.poster_path)
-          }
-          title={
-            item.original_title || item.original_name
-              ? item.original_title || item.original_name
-              : "No title"
-          }
-          rating={
-            item.vote_average === 0 ||
-            item.vote_average === undefined ||
-            !item.vote_average
-              ? "None"
-              : item.vote_average
-          }
-          info={
-            item.overview === null ||
-            item.overview === undefined ||
-            !item.overview
-              ? `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Di
+        <StyledCardWrapper key={index}>
+          <Card
+            image={
+              item.poster_path === null || item.poster_path === undefined
+                ? fallbackImage
+                : api.w500images(item.poster_path)
+            }
+            title={
+              item.original_title || item.original_name
+                ? item.original_title || item.original_name
+                : "No title"
+            }
+            rating={
+              item.vote_average === 0 ||
+              item.vote_average === undefined ||
+              !item.vote_average
+                ? "None"
+                : item.vote_average
+            }
+            info={
+              item.overview === null ||
+              item.overview === undefined ||
+              !item.overview
+                ? `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Di
                 stinctio necessitatibus debitis esse aliquid.`
-              : item.overview.slice(0, 100)
-          }
-        />
+                : item.overview.slice(0, 100)
+            }
+          />
+          <StyledFavorites>
+            <Button
+              title="Add to Favorites"
+              onClick={() => addFavorite(item)}
+            />
+            <Button
+              title="Remove from Favorites"
+              onClick={() => removeFavorite(item)}
+            />
+          </StyledFavorites>
+        </StyledCardWrapper>
       ))}
     </StyledCardContainer>
   );
